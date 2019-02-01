18ce327ddbfa5681a11a5d130a8e5a94aedc2f4d
leej2011 <0yongjaeson0@gmail.com>
2019-02-01 13:06:26 -0800

    Added unit 0 files

---
+++ Unit00/src/AsciiArt.java
@@ -0,0 +1,34 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+public class AsciiArt
+{
+	public static void main ( String[] args )
+	{
+		System.out.println("Jason Lee         1/31/19 \n\n" );
+		System.out.println("This is my animal! A cat!" );
+		System.out.println("\n\n\n\n" );
+
+		System.out.println("                                " );
+		System.out.println("               ^  ^             " );
+		System.out.println("              (owo)              " );
+		System.out.println("                |       " );
+		System.out.println("                |");
+		System.out.println("               \\|/ ");
+		System.out.println("                |");
+		System.out.println("                |");
+		System.out.println("                |");
+		System.out.println("                |");
+		System.out.println("                /\\");
+		System.out.println(" \n\n\n\nHelpFul Hints" );
+		System.out.println("\\\\ draws one backslash on the screen!\n" );
+		System.out.println("\\\" draws one double quote on the screen!\n" );
+		System.out.println("\\\' draws one single quote on the screen!\n" ); 
+	}
+}
+

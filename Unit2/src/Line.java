5c7c911e95a80d2dd539abb1160fb0b129dc77af
leej2011 <0yongjaeson0@gmail.com>
2019-02-01 14:50:14 -0800

    Finished unit 2

---
+++ Unit2/src/Line.java
@@ -0,0 +1,14 @@
+//(c) A+ Computer Science
+//www.apluscompsci.com
+//Name -
+//Date -
+
+public class Line
+{
+	public static double getSlope( float x1, float y1, float x2, float y2 )
+	{
+		double slope = (y1-y2)/(x1-x2);
+		return slope;
+	}
+
+}

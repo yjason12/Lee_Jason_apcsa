7a7f9a1301eec6a0a3df1f3e313dd3442ea3a9f7
leej2011 <0yongjaeson0@gmail.com>
2019-02-01 13:09:28 -0800

    Uploaded unit 1

---
+++ Unit1/src/StarsAndStripes.java
@@ -0,0 +1,58 @@
+//© A+ Computer Science
+//www.apluscompsci.com
+
+//Name -
+//Date -
+//Class -
+//Lab  -
+
+import static java.lang.System.*;
+
+public class StarsAndStripes
+{
+   public StarsAndStripes()
+   {
+      out.println("StarsAndStripes");
+      printTwoBlankLines();
+   }
+
+   public void printTwentyStars()
+   {
+	   System.out.println("********************");
+   
+   }
+   public void printTwentyDashes()
+   {
+	   System.out.println("--------------------");
+		
+   }
+   
+
+   public void printTwoBlankLines()
+   {
+	   System.out.println(" ");
+	   System.out.println(" ");
+   }
+   
+   public void printABigBox()
+   {	
+	   System.out.println("********************");
+	   System.out.println("*                  *");
+	   System.out.println("*                  *");
+	   System.out.println("*                  *");
+	   System.out.println("*                  *");
+	   System.out.println("*                  *");
+	   System.out.println("*                  *");
+	   System.out.println("*                  *");
+	   System.out.println("********************");
+   }
+ 
+   public void printASmallBox()
+   { 	
+	   System.out.println("**********");
+	   System.out.println("*        *");
+	   System.out.println("*        *");
+	   System.out.println("*        *");
+	   System.out.println("**********");
+   }   
+}
